d562bca890e0b8c3a76240bd0dd08cdf7cf6a6f1
pennyjiang925 <102280738+pennyjiang925@users.noreply.github.com>
2022-11-08 17:15:46 +1300

    Create constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const TOKEN_KEY = "todo-token"
+export const CLIENT_ID = process.env.TODOIST_CLIENT_ID
+export const CLIENT_SECRET = process.env.TODOIST_CLIENT_SECRET
+export const BASE_URL = "https://api.todoist.com/rest/v1"
